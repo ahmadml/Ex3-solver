224d61437ba71151b05ec9bce862b70f63901039
ahmadml <57864630+ahmadml@users.noreply.github.com>
2020-04-30 23:06:44 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,123 @@
+#include "doctest.h"
+#include "solver.hpp"
+#include <string>
+using namespace std;
+using solver::solve, solver::RealVariable, solver::ComplexVariable;
+
+TEST_CASE("test RealVariable") {
+RealVariable x;
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+CHECK(solve(2*x-5==5)==10);
+
+
+}
+
+TEST_CASE("test ComplexVariable") {
+    ComplexVariable y;
+
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+    CHECK(solve(y+5i == 2*y+3i)==0);
+
+}
